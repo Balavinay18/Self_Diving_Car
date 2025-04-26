f952d90077ca2908e64c14e3e402407071adba44
BalaVinay18 <vinayvinu0046@gmail.com>
2025-04-26 16:03:14 +0530

    2:04

--- main.js
+++ main.js
@@ -1,15 +1,64 @@
-const canvas = document.getElementById("myCanvas");
-canvas.width = 400;
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width / 2, canvas.width * 0.9); // laneCount defaults to 4
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=300;
 
-function animate() {
-    car.update();
-    canvas.height = window.innerHeight;
-    road.draw(ctx);
-    car.draw(ctx);
-    requestAnimationFrame(animate);
-}
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
+
+
+
+
+const N=100;
+const cars=generateCars(N);
+
+
+
+
+const traffic=[
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2)
+];
 
 animate();
+
+function generateCars(N){
+    const cars=[];
+    for(let i=1;i<N;i++){
+        cars.push(new Car(road.getLaneCenter(1),100,30,50,"AI"))
+
+    }
+    return cars;
+ 
+}
+
+function animate(time){
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(road.borders,[]);
+    }
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(road.borders,traffic);
+    }
+
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
+
+    carCtx.save();
+    carCtx.translate(0,-cars[0].y+carCanvas.height*0.7);
+
+    road.draw(carCtx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(carCtx,"red");
+    }
+
+    for(i=0;i<cars;i++){
+        cars[i].draw(carCtx,"blue");
+    }
+
+    carCtx.restore();
+
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,cars[0].brain);
+    requestAnimationFrame(animate);
+}
